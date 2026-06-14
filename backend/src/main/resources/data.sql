2ce26ed1ab9c06b5445416c181f1e6a12967170f
nguuyendangmanh <minhtran1122006@gmail.com>
2026-06-14 21:13:44 +0700

    Fix bug: Sửa INSERT thành INSERT IGNORE trong data.sql để tránh lỗi trùng lặp khi khởi động lại Server

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -2,7 +2,7 @@
 -- (Bỏ USE vì H2 không hỗ trợ, password đã mã hóa BCrypt của '123456')
 
 -- 1. Insert 3 tài khoản mẫu
-INSERT INTO users (phone, password, full_name, email, role) VALUES
+INSERT IGNORE INTO users (phone, password, full_name, email, role) VALUES
 ('0988888888', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Nguyễn Văn Admin', 'admin@football.vn', 'ADMIN'),
 ('0977777777', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Trần Thị Nhân Viên', 'staff@football.vn', 'STAFF'),
 ('0911111111', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Lê Khách VIP', 'vip@football.vn', 'CUSTOMER');
